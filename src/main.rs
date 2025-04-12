59470f18c509da2c7d01061161247687f9c619db
sebdal <semoda@bio.aau.dk>
2025-04-12 13:55:51 +0000

    added jaccard similarity between bins

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, path::Path};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{BufWriter, Write},
+    path::Path,
+};
 
 use anyhow::{anyhow, Context};
 use clap::Parser;
@@ -13,7 +17,7 @@ struct Args {
     #[arg(short, long, num_args(2..), required=true, help="Labels for binners in the same order as above")]
     labels: Vec<String>,
 
-    #[arg(short, long, num_args(2..), help="Path to qualities provided by CheckM2")]
+    #[arg(long, num_args(2..), help="Path to qualities provided by CheckM2")]
     qualities: Option<Vec<String>>,
 
     #[arg(short, long, num_args(1..), default_values=vec![".fa", ".fna", ".fasta"], help="Extensions to look for in directories")]
@@ -33,17 +37,37 @@ struct Contig {
     pub sequence: String,
 }
 
-impl Contig {
-    pub fn new(id: String, sequence: String) -> Self {
-        Self { id, sequence }
+pub struct Bin {
+    pub name: String,
+    pub contigs: Vec<Contig>,
+    pub completeness: Option<f64>,
+    pub contamination: Option<f64>,
+}
+
+impl Bin {
+    fn new(
+        name: impl Into<String>,
+        contigs: Vec<Contig>,
+        completeness: Option<f64>,
+        contamination: Option<f64>,
+    ) -> Self {
+        Self {
+            name: name.into(),
+            contigs,
+            completeness,
+            contamination,
+        }
     }
 }
 
-pub struct Bin {
-    name: String,
-    contigs: Vec<Contig>,
-    completeness: Option<f64>,
-    contamination: Option<f64>,
+pub struct BinIntersection {
+    pub binner_a: String,
+    pub bin_a: String,
+    pub binner_b: String,
+    pub bin_b: String,
+    pub intersection_count: usize,
+    pub union_count: usize,
+    pub jaccard_index: f64,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -61,6 +85,9 @@ fn main() -> anyhow::Result<()> {
             "The provided labels does not match the provided dirs"
         ));
     }
+    // Create output dir
+    std::fs::create_dir_all(&args.output)
+        .with_context(|| anyhow!("Could not create output directory: {}", args.output))?;
 
     let mut binner_inputs: HashMap<String, Vec<Bin>> = HashMap::new();
 
@@ -77,8 +104,8 @@ fn main() -> anyhow::Result<()> {
 
         let bin_paths = entries
             .filter_map(Result::ok)
-            .filter(|entry| {
-                let path = entry.path();
+            .map(|entry| entry.path())
+            .filter(|path| {
                 if let Some(ext_os) = path.extension() {
                     let ext = ext_os.to_string_lossy().to_lowercase();
                     allowed_exts.iter().any(|allowed| {
@@ -95,7 +122,100 @@ fn main() -> anyhow::Result<()> {
             return Err(anyhow!("No bins found in path: {}", dir));
         }
 
-        for bp 
+        let mut bins = Vec::new();
+        for bp in bin_paths.iter() {
+            let name = bp
+                .file_stem()
+                .and_then(|n| n.to_str())
+                .ok_or_else(|| anyhow!("Invalid UTF-8 in filename: {:#?}", bp))?;
+
+            let mut contigs = Vec::new();
+            let mut reader = Reader::from_path(bp).unwrap();
+
+            while let Some(record) = reader.next() {
+                let record = record?;
+
+                let rid = record.id()?;
+                let id = rid.to_string();
+                let sequence = String::from_utf8(record.owned_seq())?;
+                let c = Contig { id, sequence };
+                contigs.push(c);
+            }
+
+            let bin = Bin {
+                name: name.to_string(),
+                contigs,
+                completeness: None,
+                contamination: None,
+            };
+
+            bins.push(bin);
+        }
+        binner_inputs.insert(label.to_owned(), bins);
+    }
+
+    // Compare contig overlap between bins for different binners.
+    let mut intersections: Vec<BinIntersection> = Vec::new();
+
+    let binner_labels: Vec<_> = binner_inputs.keys().cloned().collect();
+    for (i, label1) in binner_labels.iter().enumerate() {
+        for (label2) in binner_labels.iter().skip(i + 1) {
+            if let (Some(bins1), Some(bins2)) =
+                (binner_inputs.get(label1), binner_inputs.get(label2))
+            {
+                for bin1 in bins1 {
+                    let set1: HashSet<String> = bin1.contigs.iter().map(|c| c.id.clone()).collect();
+
+                    for bin2 in bins2 {
+                        let set2: HashSet<String> =
+                            bin2.contigs.iter().map(|c| c.id.clone()).collect();
+
+                        let intersection_count = set1.intersection(&set2).count();
+                        let union_count = set1.len() + set2.len() - intersection_count;
+                        let jaccard_index = if union_count > 0 {
+                            intersection_count as f64 / union_count as f64
+                        } else {
+                            0.0
+                        };
+
+                        intersections.push(BinIntersection {
+                            binner_a: label1.clone(),
+                            bin_a: bin1.name.clone(),
+                            binner_b: label2.clone(),
+                            bin_b: bin2.name.clone(),
+                            intersection_count,
+                            union_count,
+                            jaccard_index,
+                        })
+                    }
+                }
+            }
+        }
+    }
+
+    let outpath = Path::new(&args.output).join("bin-comparisons.tsv");
+    let outfile = std::fs::File::create(outpath.clone())
+        .with_context(|| anyhow!("Could not create file: {:?}", outpath))?;
+    let mut writer = BufWriter::new(outfile);
+
+    writeln!(
+        writer,
+        "binner_a\tbin_a\tbinner_b\tbin_b\tintersection\tunion\tjaccard_index"
+    )?;
+
+    for i in intersections.iter() {
+        write!(
+            writer,
+            "{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            i.binner_a,
+            i.bin_a,
+            i.binner_b,
+            i.bin_b,
+            i.intersection_count,
+            i.union_count,
+            i.jaccard_index
+        )?;
+        writer.flush()?;
     }
 
     Ok(())
